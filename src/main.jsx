15a7d6f5b84ca188e56bdc02073241e234d18c69
WeStOn2000 <161762685+WeStOn2000@users.noreply.github.com>
2024-07-23 11:16:49 -0400

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import { createRoot } from 'react-dom/client';
 
+const title = "My First React App";
+
+const desc = "i just learned how to create a react root and render the React-Dom";
+
+
 const element = (
 <>
- <h1>My First React App</h1>
- <p>i just learned how to create a react and use React-Dom</p>
+ <h1>{title}</h1>
+ <p className='main-desc'>{desc}</p>
 </>
 )
 
