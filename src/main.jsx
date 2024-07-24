75123b8e46b66a8e9af17bc5b871112f1b4ba0f8
WeStOn2000 <161762685+WeStOn2000@users.noreply.github.com>
2024-07-24 15:48:31 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,75 @@
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-const title = "My First React App";
+const items = [
+    {
+      name: "Apples",
+      quantity: 5
+    },
+    {
+      name: "Bananas",
+      quantity: 7
+    },
+    {
+      name: "Box of Pasta",
+      quantity: 1
+    },
+    {
+      name: "Cookies",
+      quantity: 12
+    }
+  ]
 
-const desc = "i just learned how to create a react root and render the React-Dom";
+const Header = (props) => {
+   return(
+     <header>
+       <h1>{props.title}</h1>
+       <span className='total-items'>Items: {props.itemTotal}</span>
+     </header>
+   )
+ }
 
+ const Item = (props) => {
+    return(
+     <div className='item'>
+      <button className='remove-item'/>
+      <span className='item-name'>{props.name}</span>
+      <Counter quantity={props.quantity}/>
+     </div>
+    )
+ }
 
-const element = (
-<>
- <h1>{title}</h1>
- <p className='main-desc'>{desc}</p>
-</>
-)
+ const Counter = (props) => {
+    return(
+    <div className='quantity'>
+        <span className='qty-label'>QTY</span>
+        <button className='increment'>+</button>
+        <button className='decrement'>-</button>
+        <span className='quantity-amount'>{props.quantity}</span>
+    </div>
+    )
+ }
+
+ const App = (props) => {
+    return(
+        <div className='grocery-list'>
+            <Header 
+              title='My Grocery List'
+              itemTotal={1}
+            />
+
+            {/* Grocery List*/}
+            <Item 
+              name='yellow'
+              quantity={1}
+            />
+        </div>
+    )
+ }
 
 const root = createRoot( document.getElementById('root'));
-root.render(element);
+root.render(
+<StrictMode>
+  <App initialList={items} />
+</StrictMode>
+)
