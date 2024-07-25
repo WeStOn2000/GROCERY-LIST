af91c7eeb84c2773b5fa156cd48be419214047c6
WeStOn2000 <161762685+WeStOn2000@users.noreply.github.com>
2024-07-25 14:37:11 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -45,11 +45,17 @@ const Header = (props) => {
 
  const Counter = () => {
     const[quantity,setQuantity] = useState(0)
+    const incrementQuantity = () => {
+           setQuantity(quantity + 1)
+    }
+    const decrementQuantity = () => {
+        setQuantity(quantity - 1)
+    }
     return(
     <div className='quantity'>
         <span className='qty-label'>QTY</span>
-        <button className='increment'>+</button>
-        <button className='decrement'>-</button>
+        <button className='increment' onClick={incrementQuantity}>+</button>
+        <button className='decrement' onClick={decrementQuantity}>-</button>
         <span className='quantity-amount'>{quantity}</span>
     </div>
     )
